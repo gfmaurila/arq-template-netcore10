f2b8e346be4626fd3b4e7a35be21fe8f7e5152e3
Guilherme F Maurila <gfmaurila@gmail.com>
2025-10-15 21:28:36 -0300

    ini

---
+++ src/Worker/Template.Message.Kafka.Consumer.Worker/Program.cs
@@ -0,0 +1,7 @@
+using Template.Message.Kafka.Consumer.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
